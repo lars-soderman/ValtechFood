1eff8bf3144cd4e4c5d67125549b10a6ca0157d5
Lars Söderman <lars.soderman@valtech.com>
2021-09-17 10:56:19 +0200

    add script

--- index.js
+++ index.js
@@ -6,12 +6,13 @@
       var price = button.getAttribute("data-price");
 
       //Fyll datalagret vid checkout
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: "TRIGGER",
+        product: product,
+      });
 
-      window.location =
-        "thankyou.html?product=" +
-        product +
-        "&price=" +
-        price
+      window.location = "thankyou.html?product=" + product + "&price=" + price;
     },
     false
   );
